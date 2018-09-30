d459f561252ec1b26510c1512f872790ccabb5f2
dalonsogz <dalonsogz@yahoo.es>
2018-09-30 17:54:36 +0200

    Fixed Eureka server

--- eurekaclient/src/main/java/com/test/feigntest/RestTemplateConfiguration.java
+++ eurekaclient/src/main/java/com/test/feigntest/RestTemplateConfiguration.java
@@ -1,11 +1,8 @@
 package com.test.feigntest;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
  
 @Configuration
@@ -16,11 +13,5 @@ public class RestTemplateConfiguration {
     RestTemplate loadBalancedRestTemplate() {
         return new RestTemplate();
     }
-    
-//    @FeignClient("SPRINGCLOUDWEBTEST")
-//    public interface TestFeign {
-//     
-//        @RequestMapping(value="/ping", method = RequestMethod.GET)
-//        String doAlive();
-//    }
+
 }
--- eurekaserver/src/test/java/com/test/eurekaserver/EurekaservertestApplicationTests.java
+++ eurekaserver/src/test/java/com/test/eurekaserver/EurekaservertestApplicationTests.java
@@ -1,4 +1,4 @@
-package com.test.feigntest;
+package com.test.eurekaserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FeigntestApplicationTests {
+public class EurekaservertestApplicationTests {
 
 	@Test
 	public void contextLoads() {
